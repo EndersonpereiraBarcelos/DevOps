8d2daa6f637507489aa2757d6c9f0717f3326d17
endersonpb <endersonpb0@gmail.com>
2024-06-30 08:55:04 -0300

    commit inicial

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "us-west-2"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-0cf2b4e024cdb6960"
+  instance_type = "t2.micro"
+  key_name = "alura-acesso-aws"
+
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
+}
